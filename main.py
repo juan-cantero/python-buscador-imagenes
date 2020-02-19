6499ae9516285a5d7dfbcc3bab4d1d727f27025e
juan-cantero <juan.cantero@outlook.com>
2020-02-19 01:19:32 -0300

    now with threading

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import logging
 from api import PixabayAPI
+import threading
+import time
 
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
 
 carpeta_imagenes = './imagenes'
 query = 'computadoras'
-api = PixabayAPI('PONER ACÁ EL API KEY', carpeta_imagenes)
+api = PixabayAPI('15310730-4b58aad1f4a3657eafdaf8dd6', carpeta_imagenes)
 
 logging.info(f'Buscando imagenes de {query}')
-urls = api.buscar_imagenes(query, 5)
+urls = api.buscar_imagenes(query, 10)
+
 
 for u in urls:
-  logging.info(f'Descargando {u}')
-  api.descargar_imagen(u)
+  t1 = threading.Thread(target=api.descargar_imagen,args=(u,))
+  t1.start()
+
+
+
+
+
